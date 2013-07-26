4706302b1672408ca7eef846c8a4a7713fa2ad6e
chenqing <chenqing663@foxmail.com>
2013-07-26 18:13:51 +0800

    search img is ok

--- application/views/manage/index.php
+++ application/views/manage/index.php
@@ -1,18 +1,31 @@
 <div class="tabbable tabs-left" style="min-height: 400px;">
     <ul class="nav nav-tabs">
-        <li class="active"><a data-toggle="tab" href="#lA"><i class="icon-github  icon-3x"></i><p>git简易使用</p></a></li>
+        <li class="active"><a data-toggle="tab" href="#lA"><i class="icon-search  icon-3x"></i>  万能搜索</a></li>
         <li class=""><a data-toggle="tab" href="#lB"><i class="  icon-comment  icon-3x"></i>  留言吧</a></li>
-        <li class=""><a data-toggle="tab" href="#lC"><i class="icon-bookmark  icon-3x"></i>  图表汇总</a></li>
+        <li class=""><a data-toggle="tab" href="#lC"><i class="icon-github  icon-3x"></i>git简易使用</a></li>
         <li class=""><a data-toggle="tab" href="#lD"><i class="icon-coffee  icon-3x"></i>  图表汇总</a></li>
-        <li class=""><a data-toggle="tab" href="#lE"><i class="icon-spinner icon-spin icon-3x"></i>  干啥</a></li>
+        <li class=""><a data-toggle="tab" href="#lE"><i class="icon-spinner icon-spin icon-3x"></i>  再加点啥</a></li>
         <li></li>
         <li></li>
         <li></li>
     </ul>
     <div class="tab-content">
         <div id="lA" class="tab-pane active">
-            <iframe src="http://www.bootcss.com/p/git-guide/" width="100%" align="center" height="420px" frameborder="0" scrolling="auto"></iframe>
+            <div class="container">
+                <div class="row" style="min-height: 420px;">
+                    <div class="span3">
+                    </div>
+                    <div class="span6">
+                        <h1 style="min-height: 100px;" class="text-center"><img src="../assets/img/search-logo.jpg"></h1>
+                        <div class="input-append ">
+                            <input class="span5 input-xlarge" id="appendedInputButton" type="text" placeholder="搜索@host #node ~ip ">
+                            <button class="btn btn-primary" type="button">搜索</button>
+                        </div>
+                    </div>
+
+            </div>
         </div>
+            </div>
         <div id="lB" class="tab-pane">
             <!-- Duoshuo Comment BEGIN -->
             <div class="ds-thread"></div>
@@ -31,7 +44,8 @@
 
         </div>
         <div id="lC" class="tab-pane">
-            <p>What up girl, this is Section C.</p>
+            <iframe src="http://www.bootcss.com/p/git-guide/" width="100%" align="center" height="420px" frameborder="0" scrolling="auto"></iframe>
+
         </div>
         <div id="lD" class="tab-pane">
             <p>What up girl, this is Section C.</p>
