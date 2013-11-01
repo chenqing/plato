41611a9860c6f579c7c8ad0bc89d42fb43d6fa54
chenqing <chenqing663@foxmail.com>
2013-11-01 10:28:11 +0800

    change some infomation

--- application/views/manage/include/footer.php
+++ application/views/manage/include/footer.php
@@ -3,7 +3,7 @@
 <!-- footer start -->
 <footer id="footer">
 	<div class="container">
-		<p class="text-center" style="padding:10px;"><span class="icon-envelope">		</span><a href="#" style="text-decoration:none;color:#666;"><b>cpis-opt@chinacache.com</b></a></p>
+		<p class="text-center" style="padding:10px;"><span class="icon-envelope">		</span><a href="#" style="text-decoration:none;color:#666;"><b>chenqing663@foxmail.com</b></a></p>
 	</div>
 </footer>
 <!-- footer end -->
