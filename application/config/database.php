cde7e03cfedf1249edb537318e7795403e7417e4
chenqing <chenqing663@foxmail.com>
2014-05-13 13:38:37 +0800

    Update database.php
    change pass

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'Blueit_s_HF';
+$db['default']['password'] = 'yourpass';
 $db['default']['database'] = 'plato';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
